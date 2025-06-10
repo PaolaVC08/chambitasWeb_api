786cd498f6a4d48e253739c6a4e96e3e46b1916c
PaolaVC08 <zS23004729@estudiantes.uv.mx>
2025-06-10 11:15:31 -0600

    add service CRUD

--- src/main/java/com/ann/chambitasWeb/dtos/response/ServiceResponse.java
+++ src/main/java/com/ann/chambitasWeb/dtos/response/ServiceResponse.java
@@ -1,5 +1,53 @@
 package com.ann.chambitasWeb.dtos.response;
 
+import java.util.List;
+
 public class ServiceResponse {
-    
+
+    private Long idServicio;
+    private String nombre;
+    private String descripcion;
+    private String categoria;
+    private List<String> imagenesBase64;
+
+    // Getters y Setters
+    public Long getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(Long idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<String> getImagenesBase64() {
+        return imagenesBase64;
+    }
+
+    public void setImagenesBase64(List<String> imagenesBase64) {
+        this.imagenesBase64 = imagenesBase64;
+    }
 }

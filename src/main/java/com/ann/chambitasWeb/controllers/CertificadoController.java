fa2e3ff7a13ce9e7de2ce7f8c56cc04e70ccc429
PaolaVC08 <zS23004729@estudiantes.uv.mx>
2025-06-11 01:45:08 -0600

    add

--- src/main/java/com/ann/chambitasWeb/controllers/CertificadoController.java
+++ src/main/java/com/ann/chambitasWeb/controllers/CertificadoController.java
@@ -1,5 +1,5 @@
 package com.ann.chambitasWeb.controllers;
-
+//imports
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;

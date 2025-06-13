cd23ec59c3256b46e79a60660965fe500c46b778
PaolaVC08 <zS23004729@estudiantes.uv.mx>
2025-06-13 06:17:30 -0600

    update service

--- src/main/java/com/ann/chambitasWeb/controllers/ServiceController.java
+++ src/main/java/com/ann/chambitasWeb/controllers/ServiceController.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/servicios")
 public class ServiceController {
